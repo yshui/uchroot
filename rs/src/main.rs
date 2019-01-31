1433c95f2d0016eb051d563e2dfc651ea68e0ed2
Yuxuan Shui <yshuiv7@gmail.com>
2019-01-31 23:58:53 +0000

    rs: Add move mount

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,8 +1,10 @@
-use nix::unistd;
+use nix::mount::MsFlags;
 use nix::sched::CloneFlags;
+use nix::unistd;
 use std::fs::File;
 use std::io::Write;
-#[macro_use] extern crate derive_error;
+#[macro_use]
+extern crate derive_error;
 
 #[derive(Error, Debug)]
 enum Error {
@@ -27,17 +29,36 @@ fn enter_chroot(new_root: &str, cmd: &str, hostname: &str) -> Result<(), Error> 
 
     if !euid.is_root() {
         // set up uid maps
+        File::create("/proc/self/setgroups")?.write_all("deny".as_bytes())?;
         {
             let mut f = File::create("/proc/self/uid_map")?;
             let map = format!("{0} {0} 1", old_uid);
-            f.write_all(map.as_bytes());
+            f.write_all(map.as_bytes())?;
         }
         {
             let mut f = File::create("/proc/self/gid_map")?;
             let map = format!("{0} {0} 1", old_gid);
-            f.write_all(map.as_bytes());
+            f.write_all(map.as_bytes())?;
         }
     }
+
+    // Make all mount point private
+    nix::mount::mount::<str, _, str, str>(
+        None,
+        &Some("/"),
+        None,
+        MsFlags::MS_REC | MsFlags::MS_PRIVATE,
+        None,
+    )?;
+
+    // Create mount points under new_root
+    nix::mount::mount::<_, _, str, str>(
+        Some(new_root),
+        &Some(new_root),
+        None,
+        MsFlags::MS_BIND,
+        None,
+    )?;
 
     Ok(())
 }
