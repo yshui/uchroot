c4f9d0abfb555d6f75c613024bd5002af3b099d8
Yuxuan Shui <yshuiv7@gmail.com>
2019-02-01 15:40:18 +0000

    implement shell spawning

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -45,24 +45,59 @@ fn enter_chroot(new_root: &str, cmd: &str, hostname: &str) -> Result<(), Error> 
     // Make all mount point private
     nix::mount::mount::<str, _, str, str>(
         None,
-        &Some("/"),
+        "/",
         None,
         MsFlags::MS_REC | MsFlags::MS_PRIVATE,
         None,
     )?;
 
     // Create mount points under new_root
-    nix::mount::mount::<_, _, str, str>(
-        Some(new_root),
-        &Some(new_root),
-        None,
-        MsFlags::MS_BIND,
-        None,
-    )?;
+    nix::mount::mount::<_, _, str, str>(Some(new_root), new_root, None, MsFlags::MS_BIND, None)?;
+
+    // Move mount the new_root to /
+    nix::mount::mount::<_, _, str, str>(Some(new_root), "/", None, MsFlags::MS_MOVE, None)?;
 
     Ok(())
 }
+
+fn spawn_shell() -> Result<unistd::Pid, Error> {
+    match unistd::fork()? {
+        unistd::ForkResult::Child => {
+            unistd::execvp(&std::ffi::CString::new("bash").unwrap(), &[]).unwrap();
+            std::process::exit(1);
+        }
+        unistd::ForkResult::Parent { child } => Ok(child),
+    }
+}
+
+fn start_pid1() -> Result<unistd::Pid, Error> {
+    use nix::sys::wait::*;
+    let pid = unistd::fork()?;
+    match pid {
+        unistd::ForkResult::Child => {
+            let child = spawn_shell().unwrap();
+            loop {
+                let ws = wait();
+                if let Ok(WaitStatus::Exited(pid, _)) = ws {
+                    if pid == child {
+                        std::process::exit(0);
+                    }
+                }
+                if ws == Err(nix::Error::Sys(nix::errno::Errno::ECHILD)) {
+                    // All child died?
+                    std::process::exit(0);
+                }
+            }
+        }
+        unistd::ForkResult::Parent { child } => Ok(child),
+    }
+}
+
 fn main() -> Result<(), Error> {
-    enter_chroot("", "", "")?;
+    enter_chroot(&std::env::args().skip(1).next().unwrap(), "", "")?;
+
+    let child = start_pid1()?;
+    nix::sys::wait::waitpid(Some(child), None)?;
+    nix::sys::signal::kill(child, Some(nix::sys::signal::Signal::SIGKILL)).ok();
     Ok(())
 }
